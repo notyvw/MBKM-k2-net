5664b7b78f59eaceee5018c6a3a0da67d9e922c0
umaryoyono <umaryoyono@gmail.com>
2023-07-24 03:36:14 +0700

    k2net

---
+++ Http/Controllers/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Contact;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $contact = Contact::first();
+        return view('dashboard', compact('contact'));
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function adminHome()
+    {
+        $count_u = User::where('type', 'user')->count();
+        return view('adminHome', compact('count_u'));
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function managerHome()
+    {
+        return view('managerHome');
+    }
+}
